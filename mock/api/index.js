77e24341385dc06e47ea81db5cc8ab34d9fdc31d
张传龙 <zclzone@outlook.com>
2022-12-09 16:32:01 +0800

    feat: 增加密码管理业务功能

--- mock/api/index.js
+++ mock/api/index.js
@@ -1,5 +1,6 @@
 import auth from './auth'
 import user from './user'
 import post from './post'
+import password from './password'
 
-export default [...auth, ...user, ...post]
+export default [...auth, ...user, ...post, ...password]
